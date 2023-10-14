57addf83798999917ab4a3909d356725a2b00f80
Johannes Qvarford <jq.email+github@pm.me>
2023-10-14 15:31:12 +0200

    Next.js upgrade.

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const siteTitle = "Johannes Qvarford's career site"
 export default function Layout({ pageTitle, children }) {
   return <Fragment>
     <Head>
-      <title>{pageTitle} - {siteTitle}</title>
+      <title>{`${pageTitle} - ${siteTitle}`}</title>
     </Head>
     {children}
   </Fragment>;
